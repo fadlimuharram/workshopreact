462555a7b4416fcb3beb9744ffe8ea61e4e52045
Fadli <fadli@debian>
2018-07-21 13:49:18 +0700

    tambahan p

---
+++ src/components/P.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../style/P.css';
+
+const Paragraph = ()=>{
+    return (
+        <p> ini parahraph </p>
+    )
+}
+
+export default Paragraph;
+
---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../style/Button.css';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button>Free Will</button>
+      );
+  }
+}
+
+export default Button;
